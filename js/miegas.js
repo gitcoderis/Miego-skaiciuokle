b6d8bd929996876d5e56a10ab080cca0e9e5b16a
Marius <mdelininkaitis@gmail.com>
2017-11-11 16:09:20 +0200

    jquery datepicker pridetas

--- js/miegas.js
+++ js/miegas.js
@@ -1,3 +1,7 @@
+
+var valandos;
+var minutes;
+
 $(document).ready(function(){
   // var inputas = $('#inputas');
   var posY,
@@ -12,11 +16,39 @@ $(document).ready(function(){
   // $('#inputas').clone().appendTo('body');
 
   for(var i = 0; i < 50; i++){
-
-
-
-
     $('.zvaigzdutes-konteineris:first').clone().css({"bottom": posY, "right": posX}).appendTo('.zvaigzdetasDangus');
     randomNr();
    }
+
+
+   $('.timepicker').timepicker({
+    timeFormat: 'HH:mm',
+    interval: 60,
+    minTime: '0',
+    maxTime: '23:59:59',
+    defaultTime: '07:00',
+    startTime: '06:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true,
+    change: function(time){
+      valandos = time.getHours();
+      minutes = time.getMinutes();
+      var result = valandos+":"+minutes;
+      console.log(result);
+    }
 });
+
+
+
+
+
+});
+
+
+var arVeikiaDb = function(){
+  var inputoReiksme = document.getElementById('inputas').value;
+
+  var result = valandos+":"+minutes;
+  console.log(result);
+};
